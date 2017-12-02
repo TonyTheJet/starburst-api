6660e374b1b14b613dec576405a82ba14b0ad9ad
TonyTheJet <tony@tony-anderson.info>
2017-12-02 14:25:09 -0700

    getting the Eloquent database ORM included

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,17 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+	    // database
+        'db' => [
+	        'driver' => 'mysql',
+	        'host' => 'localhost',
+	        'database' => 'starburst',
+	        'username' => 'vagrant',
+	        'password' => 'vagrant',
+	        'charset'   => 'utf8',
+	        'collation' => 'utf8_unicode_ci',
+	        'prefix'    => '',
+        ]
     ],
 ];
